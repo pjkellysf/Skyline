401a2d5f5e8f2f4027d29118c1e90e3db4940288
Patrick Kelly <pjkellysf@yahoo.com>
2015-09-19 14:52:48 -0700

    Create Building.java

---
+++ Building.java
@@ -0,0 +1,46 @@
+//********************************************************************
+//  Building.java       Authors: Clifford/Kelly
+//
+//  Describes how to construct a building with windows
+//********************************************************************
+
+import java.awt.*;
+
+public class Building
+{
+   private int xval, yval, bwidth, bheight, rows, cols, spacer;
+   private Color buildingColor, windowColor;
+
+   //-----------------------------------------------------------------
+   //  Constructor: Sets up this building with the specified values.
+   //-----------------------------------------------------------------
+   public Building(int x, int y, int width, int height, Color c, int r, int z, int s, Color d)
+   {
+       xval = x;
+       yval = y;
+       bwidth = width;
+       bheight = height;
+       buildingColor = c;
+       rows = r;
+       cols = z;
+       spacer = s;
+       windowColor = d;
+   }
+
+// Draw the building
+
+   public void draw (Graphics g)
+   {
+      //Draw Building
+       g.setColor (buildingColor);
+       g.fillRect (xval, yval, bwidth, bheight);
+       
+       //Draw Windows
+       for (int j = 0; j < cols; j++) {
+           for (int i = 0; i < rows; i++) {
+                g.setColor (windowColor);
+                g.fillRect ((xval + (i*spacer)) + spacer, yval + (j*spacer) + spacer, bwidth/spacer, bheight/spacer);
+           }
+       }
+   }
+}
